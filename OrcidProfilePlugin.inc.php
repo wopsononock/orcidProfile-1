b808fef1d706c2055b057eab42d3a58873cce106
Christopher R. Maden <crism@maden.org>
2016-05-12 12:35:34 -0500

    Add settings action for OJS 3/OMP 1.2.

--- OrcidProfilePlugin.inc.php
+++ OrcidProfilePlugin.inc.php
@@ -348,7 +348,29 @@ class OrcidProfilePlugin extends GenericPlugin {
 	}
 
 	/**
-	 * Display verbs for the management interface.
+	 * @see Plugin::getActions()
+	 */
+	function getActions($request, $actionArgs) {
+		$router = $request->getRouter();
+		import('lib.pkp.classes.linkAction.request.AjaxModal');
+		return array_merge(
+			$this->getEnabled()?array(
+				new LinkAction(
+					'settings',
+					new AjaxModal(
+						$router->url($request, null, null, 'manage', null, array('verb' => 'settings', 'plugin' => $this->getName(), 'category' => 'generic')),
+						$this->getDisplayName()
+					),
+					__('manager.plugins.settings'),
+					null
+				),
+			):array(),
+			parent::getActions($request, $actionArgs)
+		);
+	}
+
+	/**
+	 * Display verbs for the management interface (OJS 2.x).
 	 */
 	function getManagementVerbs() {
 		$verbs = array();
