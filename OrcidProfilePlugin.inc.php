7065b1ad9060ea10095ea5790a33f415f774bf62
Bozana Bokan <bozana.bokan@posteo.net>
2018-02-27 17:17:49 +0100

    pkp/pkp-lib#3383 contextId parameter for plugin register and lazy load get/setEnabled

--- OrcidProfilePlugin.inc.php
+++ OrcidProfilePlugin.inc.php
@@ -30,15 +30,12 @@ define('ORCID_EMAIL_URL', 'email');
 
 class OrcidProfilePlugin extends GenericPlugin {
 	/**
-	 * Called as a plugin is registered to the registry
-	 * @param $category String Name of category plugin was registered to
-	 * @return boolean True iff plugin initialized successfully; if false,
-	 * 	the plugin will not be registered.
+	 * @copydoc Plugin::register()
 	 */
-	function register($category, $path) {
-		$success = parent::register($category, $path);
+	function register($category, $path, $mainContextId = null) {
+		$success = parent::register($category, $path, $mainContextId);
 		if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return true;
-		if ($success && $this->getEnabled()) {
+		if ($success && $this->getEnabled($mainContextId)) {
 			// Register callback for Smarty filters; add CSS
 			HookRegistry::register('TemplateManager::display', array($this, 'handleTemplateDisplay'));
 
