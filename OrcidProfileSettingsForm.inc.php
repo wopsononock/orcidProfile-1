a56655649b980de0ab95628bf2fe29e46914a326
Nils Weiher <weiher@ub.uni-heidelberg.de>
2018-05-17 09:35:16 +0200

    added consisent copyright statements, converted space to tab indents

--- OrcidProfileSettingsForm.inc.php
+++ OrcidProfileSettingsForm.inc.php
@@ -6,6 +6,7 @@
  * Copyright (c) 2015-2018 University of Pittsburgh
  * Copyright (c) 2014-2018 Simon Fraser University
  * Copyright (c) 2003-2018 John Willinsky
+ * Copyright (c) 2017-2018 University Library Heidelberg
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
  * @class OrcidProfileSettingsForm
