f8517f03aefdbd8d5d4c69e0f320891ce8d02121
withanaged <dulip.withanage@tib.eu>
2020-08-24 13:49:28 +0200

    Tests

---
+++ cypress/tests/functional/Orcid.spec.js
@@ -0,0 +1,13 @@
+/**
+ * @file cypress/tests/functional/CustomLocale.spec.js
+ *
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2000-2020 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+*/
+
+describe("Test Orcid Plugin",function () {
+	it("Contributor",function () {
+
+	})
+})
