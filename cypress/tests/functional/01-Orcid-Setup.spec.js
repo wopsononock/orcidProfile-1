3982214b45e4db363b1dbdf45fac0ab672a3e26d
withanage <dulip.withanage@gmail.com>
2020-10-11 10:44:12 +0200

    Debug travis

--- cypress/tests/functional/01-Orcid-Setup.spec.js
+++ cypress/tests/functional/01-Orcid-Setup.spec.js
@@ -16,9 +16,9 @@ describe("Test Orcid Plugin", function () {
 			cy.get('button[id="plugins-button"]').click();
 			cy.get('input[id^="select-cell-orcidprofileplugin-enabled"]').check();
 			cy.get('#component-grid-settings-plugins-settingsplugingrid-category-generic-row-orcidprofileplugin > .first_column > .show_extras').click();
-			cy.waitJQuery();
+			//cy.waitJQuery();
 			cy.get('a[id^="component-grid-settings-plugins-settingsplugingrid-category-generic-row-orcidprofileplugin-settings-button-"]').click();
-			cy.waitJQuery();
+			//cy.waitJQuery();
 			cy.get('#orcidProfileAPIPath').select(Cypress.env('orcid_apiType'));
 			cy.get('input[id^="orcidClientId-"]').clear().type(Cypress.env('orcid_clientId'));
 			cy.get('input[id^="orcidClientSecret-"]').clear().type(Cypress.env('orcid_clientSecret'));
