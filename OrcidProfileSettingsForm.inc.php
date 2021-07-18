956261f21cb86d5b823b361d3f042ff7626aa50d
withanaged <dulip.withanage@tib.eu>
2021-07-18 23:28:25 +0200

    Refactor validation

--- OrcidProfileSettingsForm.inc.php
+++ OrcidProfileSettingsForm.inc.php
@@ -16,6 +16,7 @@
 
 
 import('lib.pkp.classes.form.Form');
+import('plugins.generic.orcidProfile.classes.OrcidValidator');
 
 class OrcidProfileSettingsForm extends Form {
 
@@ -33,6 +34,8 @@ class OrcidProfileSettingsForm extends Form {
 	/** @var $plugin object */
 	var $plugin;
 
+	var $validator;
+
 	/**
 	 * Constructor
 	 * @param $plugin object
@@ -41,7 +44,8 @@ class OrcidProfileSettingsForm extends Form {
 	function __construct(&$plugin, $contextId) {
 		$this->contextId = $contextId;
 		$this->plugin =& $plugin;
-
+		$orcidValidator = new OrcidValidator($plugin);
+		$this->validator = $orcidValidator;
 		parent::__construct($plugin->getTemplateResource('settingsForm.tpl'));
 
 		if (!$this->plugin->isGloballyConfigured()) {
@@ -51,14 +55,13 @@ class OrcidProfileSettingsForm extends Form {
 		$this->addCheck(new FormValidatorPost($this));
 		$this->addCheck(new FormValidatorCSRF($this));
 		$this->addCheck(new FormValidatorCustom($this, 'orcidClientId', 'required', 'plugins.generic.orcidProfile.manager.settings.orcidClientId.error', function ($clientId) {
-			return $this->_validateClientId($clientId);
+			return $this->validator->validateClientId($clientId);
 
 		}));
 		$this->addCheck(new FormValidatorCustom($this, 'orcidClientSecret', 'required', 'plugins.generic.orcidProfile.manager.settings.orcidClientSecret.error', function ($clientSecret) {
-			return $this->validateClientSecret($clientSecret);
+			return $this->validator->validateClientSecret($clientSecret);
 
 		}));
-
 
 	}
 
@@ -116,38 +119,12 @@ class OrcidProfileSettingsForm extends Form {
 		$messages = array();
 
 		$clientId = $this->plugin->getSetting($this->_contextId, 'orcidClientId');
-		if (!$this->_validateClientId($clientId)) {
+		if (!$this->validator->validateClientId($clientId)) {
 			$messages[] = __('plugins.generic.orcidProfile.manager.settings.orcidClientId.error');
 		}
 		return $messages;
 	}
 
-	/**
-	 * @param $str
-	 * @return bool
-	 */
-	private function _validateClientId($str): bool {
-		if (preg_match('/^APP-[\da-zA-Z]{16}|(\d{4}-){3,}\d{3}[\dX]/', $str) == 1) {
-			$this->plugin->setEnabled(true);
-			return true;
-		} else {
-			$this->plugin->setEnabled(false);
-			return false;
-		}
-	}
 
-	/**
-	 * @param $str
-	 * @return bool
-	 */
-	private function validateClientSecret($str): bool {
-		if (preg_match('/^(\d|-|[a-f]){36,64}/', $str) == 1) {
-			$this->plugin->setEnabled(true);
-			return true;
-		} else {
-			$this->plugin->setEnabled(false);
-			return false;
-		}
-	}
 }
 
